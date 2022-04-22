28e1c33d890748c14aacdb2e6545d2919daf6ff4
simoncreek <simoncreek@tutanota.com>
2022-04-22 21:44:40 +0900

    New util method

--- src/cli/utils.rs
+++ src/cli/utils.rs
@@ -1,5 +1,7 @@
 use crate::error::{CedError, CedResult};
+use std::ffi::OsStr;
 use std::io::Write;
+use std::process::Stdio;
 
 pub fn write_to_stdout(src: &str) -> CedResult<()> {
     write!(std::io::stdout(), "{}", src)
@@ -30,4 +32,40 @@ pub fn read_stdin(strip_newline: bool) -> CedResult<String> {
         input.pop();
     }
     Ok(input)
+}
+
+pub fn subprocess(args: &Vec<impl AsRef<OsStr>>, process_standard_input: Option<String>) -> CedResult<()> {
+    let mut process = std::process::Command::new(&args[0])
+        .args(&args[1..])
+        .stdin(Stdio::piped())
+        .spawn()
+        .map_err(|_| {
+            CedError::CliError(format!("Failed to execute command : \"{:?}\"", &args[0].as_ref()))
+        })?;
+    let mut stdin = process
+        .stdin
+        .take()
+        .ok_or(CedError::CliError("Failed to read from stdin".to_string()))?;
+
+    if let Some(input) = process_standard_input {
+        std::thread::spawn(move || {
+            stdin
+                .write_all(input.as_bytes())
+                .expect("Failed to write to stdin");
+            });
+    } 
+
+    let output = process
+        .wait_with_output()
+        .map_err(|_| CedError::CliError("Failed to write to stdout".to_string()))?;
+    let out_content = String::from_utf8_lossy(&output.stdout);
+    let err_content = String::from_utf8_lossy(&output.stderr);
+
+    if out_content.len() != 0 {
+        write_to_stdout(&out_content)?;
+    }
+    if err_content.len() != 0 {
+        write_to_stderr(&err_content)?;
+    }
+    Ok(())
 }
