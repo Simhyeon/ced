0c7b889378ada8a383b6032d539c21e08e1ac771
simoncreek <simoncreek@tutanota.com>
2022-05-11 00:27:24 +0900

    Removed unused error

--- src/error.rs
+++ src/error.rs
@@ -1,35 +1,37 @@
+use dcsv::DcsvError;
+
 pub type CedResult<T> = Result<T, CedError>;
 
 #[derive(Debug)]
 pub enum CedError {
-    InvalidLimiter(String),
-    InvalidValueType(String),
+    CommandError(String),
+    CsvDataError(DcsvError),
+    InvalidColumn(String),
+    InvalidPageOperation(String),
+    InvalidRowData(String),
     IoError(IoErrorWithMeta),
     OutOfRangeError,
-    InsufficientRowData,
-    InvalidRowData(String),
-    InvalidColumn(String),
-    InvalidCellData(String),
-    CommandError(String),
-    InvalidPageOperation(String),
 }
 
 impl std::fmt::Display for CedError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::InvalidLimiter(txt) => write!(f,"ERR : Invalid limiter =\n{0}",txt),
-            Self::InvalidValueType(txt) => write!(f,"ERR : Invalid type =\n{0}",txt),
+            Self::CommandError(txt) => write!(f,"ERR : Invalid command call =\n{0}",txt),
+            Self::CsvDataError(err) => write!(f,"{err}"),
+            Self::InvalidColumn(txt) => write!(f,"ERR : Invalid column =\n{0}",txt),
+            Self::InvalidPageOperation(txt) => write!(f,"ERR : Invalid page operation =\n{0}",txt),
+            Self::InvalidRowData(txt) => write!(f,"ERR : Invalid row data =\n{0}",txt),
             Self::IoError(io_error) => write!(f,"ERR : IO Error =\n{0}",io_error),
             Self::OutOfRangeError => write!(f,"ERR : Index out of range"),
-            Self::InsufficientRowData => write!(f,"ERR : Insufficient row data"),
-            Self::InvalidRowData(txt) => write!(f,"ERR : Invalid row data =\n{0}",txt),
-            Self::InvalidColumn(txt) => write!(f,"ERR : Invalid column =\n{0}",txt),
-            Self::InvalidCellData(txt) => write!(f,"ERR : Invalid cell data =\n{0}",txt),
-            Self::CommandError(txt) => write!(f,"ERR : Invalid command call =\n{0}",txt),
-            Self::InvalidPageOperation(txt) => write!(f,"ERR : Invalid page operation =\n{0}",txt),
         }
     }
-} 
+}
+
+impl From<DcsvError> for CedError {
+    fn from(err: DcsvError) -> Self {
+        Self::CsvDataError(err)
+    }
+}
 
 impl CedError {
     pub fn io_error(err: std::io::Error, meta: &str) -> Self {
